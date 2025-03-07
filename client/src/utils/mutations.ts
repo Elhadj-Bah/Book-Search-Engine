73110da274e9a5a6483260458f3a30098437fed0
Elhadj Bah <[elhadjamadoubah@gmail.com]>
2025-03-06 23:28:15 -0700

    creat mutations.ts & queries.ts

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,56 @@
+import { gql } from "@apollo/client";
+
+export const ADD_BOOK = gql`
+  mutation addUser($input: UserInput) {
+    addUser(input: $input) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($input: UserInput) {
+    login(input: $input) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+`;
---
+++ client/src/utils/queries.ts
@@ -0,0 +1,38 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
