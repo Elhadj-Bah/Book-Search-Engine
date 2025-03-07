383ca7462e9df7cf01582ea367385bb3a47ad2a8
Elhadj Bah <[elhadjamadoubah@gmail.com]>
2025-03-06 23:46:42 -0700

    add README.md & remove some imports in SavedBooks.tsx

---
+++ README.md
@@ -0,0 +1,32 @@
+# Book-Search-Engine
+
+## Description
+
+MERN Stack App built with GraphQL API \_Apollo Server.
+
+## Installation
+
+1.  Clone the repository:
+
+    ```
+    git clone
+
+    ```
+
+2.  Instal depndencies: npm install
+
+3.  Set up enviromment vairables
+
+4.  Run the application by npm run start:dev
+
+## Usage
+
+1- Navigate to the root directory.
+
+2- "npm run start:dev"
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Deployed Render: deployment work still in progress...
--- client/src/pages/SavedBooks.tsx
+++ client/src/pages/SavedBooks.tsx
@@ -1,14 +1,11 @@
-import { useState, useEffect } from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
-import { getMe, deleteBook } from "../utils/API";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 import type { User } from "../models/User";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
-import { response } from "express";
 
 const SavedBooks = () => {
   const { data, loading } = useQuery(GET_ME);
