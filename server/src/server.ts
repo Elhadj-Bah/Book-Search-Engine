fae06bbf443a8b1f1e21ffb16db9e177ac7d9eaf
Elhadj Bah <[elhadjamadoubah@gmail.com]>
2025-03-21 09:02:24 -0700

    fixing path for render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { Request, Response } from "express";
 import path from "node:path";
-//const __dirname = import.meta.dirname;
+const __dirname = import.meta.dirname;
 //import { dirname } from "node:path";
 //import { fileURLToPath } from "node:url";
 //const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -43,12 +43,12 @@ const startApolloServer = async () => {
   // if we're in production, serve client/build as static assets
 
   if (process.env.NODE_ENV === "production") {
-    //app.use(express.static(path.join(__dirname, "../client/dist")));
-    app.use(express.static(path.join("../../client/dist")));
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
+    // app.use(express.static(path.join("../../client/dist")));
 
     app.get("*", (_req: Request, res: Response) => {
-      //  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-      res.sendFile(path.join("../../client/dist/index.html"));
+      res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+      //res.sendFile(path.join("../../client/dist/index.html"));
     });
   }
 
